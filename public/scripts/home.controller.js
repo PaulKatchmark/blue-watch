b84d77fed3d8e1c8ddb3b043828d88f8a9113e04
Kelly ONeil <kelly.brianne.oneil@gmail.com>
2016-11-16 11:40:25 -0600

    working on getting map to show, api key not activated

--- public/scripts/home.controller.js
+++ public/scripts/home.controller.js
@@ -2,8 +2,15 @@ angular.module('blueWatchApp')
 .controller('HomeController', HomeController);
 
 function HomeController($http, $location) {
+  var controller = this;
 
+  var mapOptions = {
+    zoom: 4,
+    center: new google.maps.LatLng(40.0000, -98.0000),
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  }
 
+    controller.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
 
 
