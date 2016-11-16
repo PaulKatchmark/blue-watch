efc17ba02968e67689a5e5044d389bf96b924c53
PaulKatchmark <paulkatchmark@gmail.com>
2016-11-16 15:28:19 -0600

    added homecontroller to otherwise config option

--- public/scripts/home.controller.js
+++ public/scripts/home.controller.js
@@ -2,21 +2,22 @@ angular.module('blueWatchApp')
 .controller('HomeController', HomeController);
 
 function HomeController($http, $location) {
+  console.log('Home controller');
   var controller = this;
 
 
-this.searchResources = function(search){
-  console.log(search);
-}
+  this.searchResources = function(search){
+    console.log(search);
+  }
 
   var mapOptions = {
     zoom: 4,
     center: new google.maps.LatLng(40.0000, -98.0000),
     mapTypeId: google.maps.MapTypeId.TERRAIN
   };
-  
-    controller.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-console.log(controller.map);
+  controller.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  console.log(controller.map);
 
 };
