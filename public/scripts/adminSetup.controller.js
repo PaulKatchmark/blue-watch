b27dd869715a90093420975cd2f17332681340bd
Levy Kohout <levykohout@users.noreply.github.com>
2016-11-16 17:45:57 -0600

    new admin user working

--- public/scripts/adminSetup.controller.js
+++ public/scripts/adminSetup.controller.js
@@ -1,12 +1,25 @@
 angular.module('blueWatchApp')
 .controller('AdminSetupController', AdminSetupController);
 
-function AdminSetupController($http, $location) {
+function AdminSetupController($http) {
+console.log('AdminSetupController loaded!');
+
+var admin = this;
+
+admin.addNewUser = function(email, password){
+    var data = {
+        email:email,
+        password:password
+    };
+console.log('da')
+$http.post('admin', data).then(function(response){
+    console.log(response);
+});
+
+
+}; //End of addNewUser
 
 
 
 
-
-
-
-};
+}; // End of AdminSetupController
