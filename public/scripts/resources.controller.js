824c2ecc405bb67fc27dc841e4c6b8ae7cf42424
Julie S Mike <juliesmike@gmail.com>
2016-11-17 12:58:20 -0600

    Start on resources

--- public/scripts/resources.controller.js
+++ public/scripts/resources.controller.js
@@ -2,5 +2,26 @@ angular.module('blueWatchApp')
 .controller('ResourcesController', ResourcesController);
 
 function ResourcesController($http, $location) {
-  
+  var controller = this;
+
+  //controller to create new resource
+  controller.createresource = function() {
+    var body = {
+    company:  controller.company,
+    description: controller.description,
+    contact: controller.contact,
+    website: controller.website,
+    street: controller.street,
+    city: controller.city,
+    state: controller.state,
+    zip: controller.zip
+  };
+      $http.post('/resource', body
+    ).then(function(){
+      $location.path('/resource');
+    }, function(error) {
+      console.log('error creating resource', error);
+    });
+  };
+
 }
--- routes/resources.js
+++ routes/resources.js
@@ -1,12 +1,26 @@
 const express = require('express');
-
-//can't use same const name unlike variable
+//can't use same const name unlike variable - (JSM - who made this comment & what?)
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const router = require('express').Router();
-const passport = require('passport');
+const mongoose = require('mongoose');
+const Resources = require('../models.resourceSchema.js');
+const Admin = require('../adminSchema.js');
+const Category = require('../categorySchema.js');
 
 
+//post new resources and attach category
+router.post('/', function(req,res){
+  var resource = new Resource(req.body);
+  resource.categoryId = req.category.id;
+
+  resource.save().then(function(resource){
+      res.sendStatus(201);
+    }).catch(function(err){
+      console.log('error in post resource', err);
+      res.sendStatus(500);
+  });
+});
 
 module.exports = router;
