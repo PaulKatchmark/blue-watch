65691d0299bb0749b275c3c86216afe88e63873f
Levy Kohout <levykohout@users.noreply.github.com>
2016-11-16 19:18:57 -0600

    admin setup GET/POST working

--- public/scripts/adminSetup.controller.js
+++ public/scripts/adminSetup.controller.js
@@ -5,6 +5,7 @@ function AdminSetupController($http) {
 console.log('AdminSetupController loaded!');
 
 var admin = this;
+admin.usersArray = [];
 
 admin.addNewUser = function(email, password){
     var data = {
@@ -12,13 +13,32 @@ admin.addNewUser = function(email, password){
         password:password
     };
 console.log('da')
-$http.post('admin', data).then(function(response){
-    console.log(response);
+$http.post('/admin', data).then(function(response){
+    console.log('successfully added a new user', response);
 });
 
 
 }; //End of addNewUser
 
+admin.getUser = function(){
+    $http.get('/admin').then(function(response){
+        console.log('successfully get users',response);
+        admin.usersArray = response.data;
+
+    });
+}//End of getUser
+
+admin.getUser();
+
+admin.updateUser = function(email, password, id){
+    var data = {
+        email:email,
+        password:password
+    };
+    $http.put('/admin/'+id, data).then(function(response){
+        console.log('successfully updated the user', response);
+    });
+}; //End of updateUser
 
 
 
