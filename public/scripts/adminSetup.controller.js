3feca87c148a1724b4ab94f7341681357b090baf
Levy Kohout <levykohout@users.noreply.github.com>
2016-11-17 13:09:28 -0600

    added update admin

--- public/scripts/adminSetup.controller.js
+++ public/scripts/adminSetup.controller.js
@@ -6,6 +6,7 @@ console.log('AdminSetupController loaded!');
 
 var admin = this;
 admin.usersArray = [];
+admin.capturedId = '';
 
 admin.addNewUser = function(email, password){
     var data = {
@@ -15,12 +16,13 @@ admin.addNewUser = function(email, password){
 console.log('da')
 $http.post('/admin', data).then(function(response){
     console.log('successfully added a new user', response);
+    admin.getUsers();
 });
 
 
 }; //End of addNewUser
 
-admin.getUser = function(){
+admin.getUsers = function(){
     $http.get('/admin').then(function(response){
         console.log('successfully get users',response);
         admin.usersArray = response.data;
@@ -28,17 +30,33 @@ admin.getUser = function(){
     });
 }//End of getUser
 
-admin.getUser();
+admin.getUsers();
 
-admin.updateUser = function(email, password, id){
+admin.captureId = function(id){
+admin.capturedId=id;
+console.log('capturedId ', admin.capturedId);
+};//End of captureId
+
+admin.updateUser = function(email, password){
     var data = {
         email:email,
         password:password
     };
-    $http.put('/admin/'+id, data).then(function(response){
+    $http.put('/admin/'+ admin.capturedId, data).then(function(response){
         console.log('successfully updated the user', response);
+        admin.getUsers();
     });
 }; //End of updateUser
+
+
+
+admin.deleteUser = function(id){
+
+    $http.delete('/admin/'+id).then(function(response){
+        console.log('successfully deleted user', response);
+        admin.getUsers();
+    });
+}; //End of deleteUser
 
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,12 @@ const Admin = require('../models/adminSchema');
 
 router.post('/', function(req, res) {
   console.log('registering new admin');
+
+  //Don't delete this needed for super_admin
+  // if (req.user.role !== 'super_admin'){
+  //     return res.sendStatus(403);
+  // }
+
   const admin = new Admin({email: req.body.email, password: req.body.password});
 
   admin.save().then(function() {
@@ -27,11 +33,54 @@ router.get('/', function(req, res) {
 //finds all users inside admin database
   Admin.find({}).then(function(people){
         res.send(people);
-        
+
   }).catch(function(err){
     console.log('Error in /register', err);
     res.sendStatus(500);
   });
 });
+
+router.put('/:id', function(req, res) {
+  console.log('updating admin user');
+  var id = req.params.id;
+  console.log(id);
+
+  Admin.findById(id, function(err, user){
+      if (err){
+        res.sendStatus(500);
+        return;
+      }
+      //set values
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    user.save(function (err, updatedUser){
+      if (err){
+        res.sendStatus(500);
+        return;
+      }
+      res.send(updatedUser);
+    });  
+  });
+});
+
+
+
+router.delete('/:id', function(req, res) {
+  console.log('deleting admin user');
+  var id = req.params.id;
+  console.log(id);
+
+//finds all users inside admin database
+  Admin.remove({ "_id" : id }).then(function(people){
+        res.sendStatus(200);
+
+  }).catch(function(err){
+    console.log('Error in /register', err);
+    res.sendStatus(500);
+  });
+});
+
+
 
 module.exports = router;
