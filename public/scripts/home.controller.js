8f6689b0d616366268bd86bd2cb5b979ad0a30f0
Levy Kohout <levykohout@users.noreply.github.com>
2016-11-18 23:10:43 -0600

    get bounds functionality setup and latitude and longitude sourced directly in database

--- public/scripts/home.controller.js
+++ public/scripts/home.controller.js
@@ -1,192 +1,183 @@
 angular.module('blueWatchApp')
-.controller('HomeController', HomeController);
+    .controller('HomeController', HomeController);
 
 function HomeController($http, $location) {
-  console.log('Home controller');
-  var controller = this;
+    console.log('Home controller');
+    var controller = this;
 
-  controller.globalMarkers;
+    controller.globalMarkers;
+    controller.resources;
+    controller.selectedCategoryArray;
+    //sets where the map is located, type and zoom
 
+    var mapOptions = {
+        center: new google.maps.LatLng(40.0000, -98.0000),
+        zoom: 4,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
 
-controller.resources;
-controller.selectedCategoryArray;
+        panControl: true,
+        mapTypeControl: false,
+        panControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_CENTER
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.LARGE,
+            position: google.maps.ControlPosition.RIGHT_CENTER
+        },
+        scaleControl: false,
+        streetViewControl: false,
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_CENTER
+        }
+    };
 
-//loads all the resources on page load
-  controller.getResources = function(){
-
-      $http.get('/resource').then(function(response){
-          controller.resources=response.data;
-          console.log(controller.resources);
-      });
-
-  };
-  controller.getResources();
-
-
-
-
-  controller.searchResources = function(search){
-    console.log(search);
-  }
-
-//changes the category list to list of resources from selected category
-controller.change = {categoryList:false};
-controller.change = {selectedCateogry:false};
-  controller.expandCategory = function(category){
-    controller.selectedCategoryArray = [];
-    //will take in what the user wants so it can be listed on the DOM
-    controller.resources.forEach(function(resource){
-      if (resource.category.categoryName === category){
-        controller.selectedCategoryArray.push(resource);
-      }
-    });
-      //this hides the categoryList and shows the list of selected categories
-    controller.change.categoryList = !controller.change.categoryList;
-    controller.change.selectedCateogry = !controller.change.selectedCateogry;
-  }
-
-  controller.backCategories = function(category){
-    controller.search="";
-    controller.change = {categoryList:false};
-    controller.change = {selectedCateogry:false};
-  }
-
-//examples of locations
-var cities = [
-    {
-        city : 'Toronto',
-        desc : 'Wellness',
-        lat : 43.7000,
-        long : -79.4000
-    },
-    {
-        city : 'New York',
-        desc : 'Counseling',
-        lat : 40.6700,
-        long : -73.9400
-    },
-    {
-        city : 'Chicago',
-        desc : 'Family therapy',
-        lat : 41.8819,
-        long : -87.6278
-    },
-    {
-        city : 'Los Angeles',
-        desc : 'Financial Services',
-        lat : 34.0500,
-        long : -118.2500
-    },
-    {
-        city : 'Las Vegas',
-        desc : 'Doctor',
-        lat : 36.0800,
-        long : -115.1522
-    }
-];
-
-//sets where the map is located, type and zoom
-  var mapOptions = {
-    zoom: 4,
-    center: new google.maps.LatLng(40.0000, -98.0000),
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-  };
-
-
-//creates the map
+    //creates the map
     controller.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-//array of all the markers
-    controller.markers = [];
 
-    var infoWindow = new google.maps.InfoWindow();
 
-    var createMarker = function (info){
+    //loads all the resources on page load
+    controller.getResources = function() {
 
-        var marker = new google.maps.Marker({
-            map: controller.map,
-            position: new google.maps.LatLng(info.lat, info.long),
-            title: info.city
+        $http.get('/resource').then(function(response) {
+            controller.resources = response.data;
+
+            //array of all the markers
+            controller.markers = [];
+
+
+            controller.resources.forEach(function(info) {
+
+                var myResult = {};
+                //get address from resource
+                console.log('address', info.street + info.city + info.state + info.zip);
+                var address = info.street + ' ' + info.city + ' ' + info.state + ' ' + info.zip;
+                //call geocode to convert to lat/long
+                var geocoder = new google.maps.Geocoder();
+                geocoder.geocode({
+                    address: address
+                }, function(results, status) {
+
+                    if (status == google.maps.GeocoderStatus.OK) {
+
+                        info.lat = results[0].geometry.location.lat();
+                        info.long = results[0].geometry.location.lng();
+
+                        console.log('Geocode Result', info.lat, info.long);
+
+                        //creates markers
+                        createMarker(info.lat, info.long);
+
+                    } else { // if status value is not equal to "google.maps.GeocoderStatus.OK"
+
+                        // warning message
+                        alert("The Geocode was not successful for the following reason: " + status);
+
+                    }
+
+                }); //End of geocode
+
+                //create marker
+                function createMarker(latinfo, lnginfo) {
+                    console.log('Geocode Result', latinfo, lnginfo);
+                    var marker = new google.maps.Marker({
+                        map: controller.map,
+                        position: new google.maps.LatLng(latinfo, lnginfo),
+                        title: info.city,
+                        visible: true
+                    });
+
+                    marker.content = '<div class="infoWindowContent">' + info.description + '</div>';
+
+                    var infoWindow = new google.maps.InfoWindow();
+
+                    controller.openInfoWindow = function(event, selectedMarker) {
+                            event.preventDefault();
+                            google.maps.event.trigger(selectedMarker, 'click');
+                        }
+                        //opens bubble on marker click
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infoWindow.setContent('<p>' + marker.title + ': ' + marker.content + '</p>');
+                        infoWindow.open(controller.map, marker);
+                    });
+
+                    google.maps.event.addListener(controller.map, 'idle', function() {
+                        controller.map.getBounds().contains(marker.getPosition());
+                        console.log('city in bounds', info.city, controller.map.getBounds().contains(marker.getPosition()));
+                    });
+
+                }; //End of createMarker
+
+
+
+
+            }); //End of for each
+
+
         });
-        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
 
-        google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent('<p>' + marker.title + ': ' + marker.content + '</p>');
-            infoWindow.open(controller.map, marker);
+    }; //End of getResources
+    controller.getResources();
+    console.log('controller.resources before mapping', controller.resources);
+
+    //changes the category list to list of resources from selected category
+    controller.change = {
+        categoryList: false
+    };
+    controller.change = {
+        selectedCateogry: false
+    };
+    controller.expandCategory = function(category) {
+        controller.selectedCategoryArray = [];
+
+        //will take in what the user wants so it can be listed on the DOM
+        controller.resources.forEach(function(resource) {
+            if (resource.category.categoryName === category) {
+                controller.selectedCategoryArray.push(resource);
+            }
         });
-
-        var myMarker = {
-          marker: marker,
-          visible: false,
-          // ...
-        }
-        controller.globalMarkers = myMarker;
-        controller.markers.push(myMarker);
-
-
-        // console.log('checkInOrOut() ', checkInOrOut(marker));
-        // checkInOrOut(marker);
-
+        //this hides the categoryList and shows the list of selected categories
+        controller.change.categoryList = !controller.change.categoryList;
+        controller.change.selectedCateogry = !controller.change.selectedCateogry;
     }
 
-  google.maps.event.addListener(controller.map, 'idle', function() {
-      // console.log(checkInOrOut(controller.globalMarkers));
-      // checkInOrOut(controller.globalMarkers);
-      // for (i = 0; i < cities.length; i++){
-      //     createMarker(cities[i]);
-      // }
-      console.log('checkInOrOut() ', checkInOrOut(controller.markers));
-      checkInOrOut(controller.markers);
-
-    });
-    function checkInOrOut(markers){
-      markers.forEach(function(resourceMarker){
-        // console.log('resourceMarker ', resourceMarker);
-      var marker = resourceMarker.marker;
-
-     controller.map.getBounds().contains(marker.getPosition());
-     console.log('marker ', marker);
-      });
-      }
-
-    //
-    for (i = 0; i < cities.length; i++){
-        createMarker(cities[i]);
+    controller.backCategories = function(category) {
+        controller.search = "";
+        controller.change = {
+            categoryList: false
+        };
+        controller.change = {
+            selectedCateogry: false
+        };
     }
 
-    controller.openInfoWindow = function(event, selectedMarker){
-        event.preventDefault();
-        google.maps.event.trigger(selectedMarker, 'click');
+    controller.searchAddress = function() {
+
+        var addressInput = document.getElementById('address-input').value;
+
+        var geocoder = new google.maps.Geocoder();
+
+        geocoder.geocode({
+            address: addressInput
+        }, function(results, status) {
+
+            if (status == google.maps.GeocoderStatus.OK) {
+
+                var myResult = results[0].geometry.location;
+
+                controller.map.setCenter(myResult);
+
+                controller.map.setZoom(15);
+
+            } else { // if status value is not equal to "google.maps.GeocoderStatus.OK"
+
+                // warning message
+                alert("The Geocode was not successful for the following reason: " + status);
+
+            }
+
+        });
     }
-
-
-  controller.searchAddress = function() {
-
-  var addressInput = document.getElementById('address-input').value;
-
-  var geocoder = new google.maps.Geocoder();
-
-  geocoder.geocode({address: addressInput}, function(results, status) {
-
-    if (status == google.maps.GeocoderStatus.OK) {
-
-      var myResult = results[0].geometry.location;
-
-      controller.map.setCenter(myResult);
-
-      controller.map.setZoom(15);
-
-    }  else { // if status value is not equal to "google.maps.GeocoderStatus.OK"
-
-    // warning message
-    alert("The Geocode was not successful for the following reason: " + status);
-
-  }
-
-  });
-}
-
-
-
 
 };
