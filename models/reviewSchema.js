c1f815b248ba2b1a5eb285b7dfc4386a028210bc
Kelly ONeil <kelly.brianne.oneil@gmail.com>
2016-11-23 10:05:43 -0600

    fix schema

--- models/reviewSchema.js
+++ models/reviewSchema.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const reviewSchema = new Schema({
   resource_id: {type: Schema.Types.ObjectId, ref: 'resource'},
   rating: Number,
-  comments: {type: String, maxlength: 200}
+  comments: {type: String, maxlength: 200},
   approved: {type: Boolean, default: false}
 });
 
