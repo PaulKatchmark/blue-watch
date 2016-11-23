fe3b99ecf8f803d676052e13aeac9292d48f606d
Levy Kohout <levykohout@users.noreply.github.com>
2016-11-23 17:56:35 -0600

    review approval delete/put request setup and fixed resource id in review submission

--- routes/reviews.js
+++ routes/reviews.js
@@ -38,4 +38,44 @@ Review.find({ "resource_id" : id }).then(function(review){
 });
 });
 
+router.delete('/:id', function(req, res) {
+  console.log('deleting a review');
+  var id = req.params.id;
+  console.log(id);
+
+//finds all users inside admin database
+  Review.remove({ "_id" : id }).then(function(review){
+        res.sendStatus(200);
+
+  }).catch(function(err){
+    console.log('Error in deleting resource', err);
+    res.sendStatus(500);
+  });
+});
+
+router.put('/:id', function(req, res) {
+  console.log('approved review');
+  var id = req.params.id;
+  console.log(req.body);
+
+  Review.findById(id, function(err, review){
+      if (err){
+        res.sendStatus(500);
+        return;
+      }
+      //set values
+      review.comments = req.body.comments;
+      review.approved = true
+
+    review.save(function (err, approvedReview){
+      if (err){
+        res.sendStatus(500);
+        return;
+      }
+      res.send(approvedReview);
+    });
+  });
+});
+
+
 module.exports = router;
