ee936d82a545264f7a3e19eb446f9008bb4a4b48
Levy Kohout <levykohout@users.noreply.github.com>
2016-12-01 13:17:22 -0600

    nav bar user email correct display and hide full permission admin tabs

--- routes/login.js
+++ routes/login.js
@@ -5,5 +5,16 @@ router.post('/', passport.authenticate('local'), function(req, res){
   res.sendStatus(200);
 });
 
+router.get('/info', function(req, res) {
+  if (req.isAuthenticated()){
+    var user = {
+      email: req.user.email,
+      accessLevel:req.user.accessLevel
+    }
+    return res.send(user);
+  }
+  res.sendStatus(401);
+});
+
 
 module.exports = router;
